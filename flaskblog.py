101233530edeac3397a26bbe69972a2edfaec2cf
Eugene Jemwa <ejemwa04@gmail.com>
2018-12-19 16:15:37 +0200

    forms

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,9 @@
-from flask import Flask , render_template , url_for
+from flask import Flask , render_template , url_for, flash, redirect
+from forms import RegisrationForm,LoginForm 
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'b99a28f432'
+
 posts=[
 {
     'author':'eugene jemwa',
@@ -27,7 +31,21 @@ def home():
 
 @app.route("/about")
 def about():
-    return render_template('about.html',title='about')    
+    return render_template('about.html',title='about') 
+
+@app.route("/register",methods = ['GET','POST'])
+def register():
+    form = RegisrationForm() 
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='register', form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html',title='login', form=form) 
+    
     
     
     
